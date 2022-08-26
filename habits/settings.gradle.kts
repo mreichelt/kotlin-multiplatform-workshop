65b75a9f8af0395d5b78f7cacc72f3d5de9ddc46
Marc Reichelt <mcreichelt@gmail.com>
2022-08-26 22:07:15 +0200

    Add hello world project using KMM plugin

---
+++ habits/settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Habits"
+include(":androidApp")
+include(":shared")
---
+++ habits/shared/src/commonMain/kotlin/com/example/habits/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.habits
+
+expect class Platform() {
+    val platform: String
+}

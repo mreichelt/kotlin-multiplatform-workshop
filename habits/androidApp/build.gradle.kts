5bf20359cddb5e45cc0faaad2d489a1252bb1542
Marc Reichelt <mcreichelt@gmail.com>
2022-09-08 21:59:23 +0200

    Add Compose to Android project

--- habits/androidApp/build.gradle.kts
+++ habits/androidApp/build.gradle.kts
@@ -17,11 +17,23 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.0"
+    }
 }
 
 dependencies {
     implementation(project(":shared"))
+
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+
+    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.compose.material:material:1.2.1")
+    implementation("androidx.compose.ui:ui-tooling:1.2.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
 }
--- habits/androidApp/src/main/java/com/example/habits/android/MainActivity.kt
+++ habits/androidApp/src/main/java/com/example/habits/android/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.example.habits.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.habits.Greeting
-import android.widget.TextView
-
-fun greet(): String {
-    return Greeting().greeting()
-}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        setContent {
+            MaterialTheme {
+                Main()
+            }
+        }
     }
+}
+
+@Composable
+fun Main() {
+    Row(Modifier.fillMaxHeight()) {
+        Text(
+            text = greet(),
+            style = MaterialTheme.typography.body1,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterVertically)
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun MainPreview() {
+    Main()
+}
+
+fun greet(): String {
+    return Greeting().greeting()
 }
